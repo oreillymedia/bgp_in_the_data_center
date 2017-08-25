32e626ec7efea729c6605507c290f02d8b71102a
Pete Lumbis <plumbis@users.noreply.github.com>
2017-08-25 10:52:35 -0700

    corrects git repo

--- helper_scripts/bgpdc-clone.sh
+++ helper_scripts/bgpdc-clone.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo su - cumulus -c '\
-git clone git@github.com:oreillymedia/bgp_in_the_data_center.git bgp_conf; \
+git clone https://github.com/oreillymedia/bgp_in_the_data_center.git bgp_conf; \
 '
